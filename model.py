66c8dba134f3221522c47cb75ece17ea1082a7f4
Gautier Dagan <s2234411@ed.ac.uk>
2022-02-08 18:05:31 +0000

    feat: implement accuracy calculations

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 import pickle
 from collections import OrderedDict
+from typing import Tuple
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
+
+from object_attributes import OBJECT_ATTRIBUTES
 
 
 class Piglet(pl.LightningModule):
@@ -19,6 +22,7 @@ class Piglet(pl.LightningModule):
         action_embedding_size=10,
         none_object_index=102,
         reverse_object_mapping_dir="data",
+        tie_weights=True,
     ):
         """
         Args:
@@ -30,7 +34,8 @@ class Piglet(pl.LightningModule):
           num_attributes: number of attributes per object
           action_embedding_size: max embedding index the action
           none_object_index: index of the none object
-          reverse_object_mapping: path to the reverse object mapping
+          reverse_object_mapping_dir: path to the reverse object mapping
+          tie_weights: if true, the weights of the object decoder are tied to the weights of the object encoder
         """
         super().__init__()
         self.save_hyperparameters()
@@ -43,7 +48,10 @@ class Piglet(pl.LightningModule):
         self.num_attributes = num_attributes
         self.action_embedding_size = action_embedding_size
         self.none_object_index = none_object_index  # mask object tensor with zeroes if object = none_object_index
+        self.tie_weights = tie_weights
         self.activation = nn.Tanh()
+
+        assert len(OBJECT_ATTRIBUTES) == self.num_attributes
 
         # mask for embedding layer output -> based on position
         # mask probability to 0 for other attributes when looking at a specific attribute
@@ -52,11 +60,11 @@ class Piglet(pl.LightningModule):
             f"{reverse_object_mapping_dir}/reverse_object_mapping.pkl"
         )
         with open(reverse_object_mapper_path, "rb") as f:
-            reverse_object_mapper = pickle.load(f)
+            self.reverse_object_mapper = pickle.load(f)
         indexes = torch.tensor(
             [
                 (pos, index)
-                for pos, index_mapper in reverse_object_mapper.items()
+                for pos, index_mapper in self.reverse_object_mapper.items()
                 for index, _ in index_mapper.items()
             ]
         )
@@ -102,6 +110,9 @@ class Piglet(pl.LightningModule):
         )
 
         self.object_decoder_output_layer = nn.Linear(hidden_size, object_embedding_size)
+
+        if tie_weights:
+            self.object_decoder_output_layer.weight = self.object_embedding_layer.weight
 
     def forward(self, object_inputs, action_inputs):
         # always four "objects" per example 2 before and 2 after
@@ -153,68 +164,129 @@ class Piglet(pl.LightningModule):
         h_o_post = self.activation(h_o_post)
 
         # map sequence to embedding dimension
-        h_o_post_ouput = (
-            self.object_decoder_output_layer(h_o_post) * self.mask_embedding_layer
-        )
+        h_o_post = self.object_decoder_output_layer(h_o_post)
 
         # returns predicted sequence of object attributes
         # torch.Size([batch_size*2, num_attributes, object_embedding_size])
-        return h_o_post_ouput
+        return h_o_post
 
-    def training_step(self, batch, batch_idx):
+    def calculate_object_attribute_loss_and_predictions(
+        self, h_o_post, objects_labels_post
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+
+        # mask the embedding layer output to only allow predictions for valid attributes at each position
+        h_o_post = torch.where(
+            self.mask_embedding_layer == 1,
+            h_o_post,
+            torch.tensor(float("-inf")),
+        )
+
+        loss = F.cross_entropy(
+            h_o_post.reshape(-1, self.object_embedding_size),
+            objects_labels_post.flatten(),
+            reduction="none",
+        )
+
+        none_mask = torch.ones_like(objects_labels_post)
+        # first column of the object vector is the indexed (name) of the object
+        # if index == self.none_object_index then the object is not present
+        none_mask *= (objects_labels_post[:, :, 0] != self.none_object_index).unsqueeze(
+            2
+        )
+
+        # average over actual objects (not none objects)
+        avg_loss = (loss * none_mask.flatten()).sum() / (none_mask.sum() + 1e-12)
+
+        # get top 1 prediction for each object attribute
+        predictions = h_o_post.argmax(2)
+
+        return avg_loss, predictions
+
+    def training_step(self, batch, batch_idx) -> torch.tensor:
         objects = batch["objects"]
         actions = batch["actions"]
 
         # the objects vector containst four dimensions (pre_0, pre_1, post_0, post_1)
         # here we select the labels for both objects post action
-        objects_post = objects[:, [2, 3], :]
+        objects_labels_post = objects[:, [2, 3], :]
 
         # encode objects and actions, apply action, and predict resulting object attributes
         h_o_post_ouput = self(objects, actions)
 
-        loss = F.cross_entropy(
-            h_o_post_ouput.reshape(-1, self.object_embedding_size),
-            objects_post.flatten(),
-            reduction="none",
+        avg_loss, _ = self.calculate_object_attribute_loss_and_predictions(
+            h_o_post_ouput, objects_labels_post
         )
-
-        none_mask = torch.ones_like(objects_post)
-
-        # first column of the object vector is the indexed (name) of the object
-        # if index == self.none_object_index then the object is not present
-        none_mask *= (objects_post[:, :, 0] != self.none_object_index).unsqueeze(2)
-
-        # average over actual objects (not none objects)
-        avg_loss = (loss * none_mask.flatten()).sum() / (none_mask.sum() + 1e-12)
         self.log("train/loss", avg_loss)
         return avg_loss
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx) -> None:
         objects = batch["objects"]
         actions = batch["actions"]
 
         # the objects vector containst four dimensions (pre_0, pre_1, post_0, post_1)
+        objects_labels_pre = objects[:, [0, 1], :]
+
         # here we select the labels for both objects post action
-        objects_post = objects[:, [2, 3], :]
+        objects_labels_post = objects[:, [2, 3], :]
 
         # encode objects and actions, apply action, and predict resulting object attributes
         h_o_post_ouput = self(objects, actions)
 
-        loss = F.cross_entropy(
-            h_o_post_ouput.reshape(-1, self.object_embedding_size),
-            objects_post.flatten(),
-            reduction="none",
+        avg_loss, predictions = self.calculate_object_attribute_loss_and_predictions(
+            h_o_post_ouput, objects_labels_post
         )
-        none_mask = torch.ones_like(objects_post)
-
-        # first column of the object vector is the indexed (name) of the object
-        # if index == self.none_object_index then the object is not present
-        none_mask *= (objects_post[:, :, 0] != self.none_object_index).unsqueeze(2)
-
-        # average over actual objects (not none objects)
-        avg_loss = (loss * none_mask.flatten()).sum() / (none_mask.sum() + 1e-12)
-
         self.log("val/loss", avg_loss)
+        return (predictions, objects_labels_pre, objects_labels_post)
 
-    def configure_optimizers(self):
+    def validation_epoch_end(self, validation_step_outputs) -> None:
+        object_attribute_predictions_post = []
+        object_attribute_labels_pre = []
+        object_attribute_labels_post = []
+        for (preds, labels_pre, labels_pro) in validation_step_outputs:
+            object_attribute_predictions_post.append(preds)
+            object_attribute_labels_pre.append(labels_pre)
+            object_attribute_labels_post.append(labels_pro)
+
+        # concat all predictions and labels into 2D tensor
+        object_attribute_predictions_post = torch.stack(
+            object_attribute_predictions_post
+        ).reshape(-1, self.num_attributes)
+        object_attribute_labels_pre = torch.stack(object_attribute_labels_pre).reshape(
+            -1, self.num_attributes
+        )
+        object_attribute_labels_post = torch.stack(
+            object_attribute_labels_post
+        ).reshape(-1, self.num_attributes)
+
+        # ignore where label of object is none_object_index
+        object_attribute_predictions_post = object_attribute_predictions_post[
+            object_attribute_labels_pre[:, 0] != self.none_object_index
+        ]
+        object_attribute_labels_pre = object_attribute_labels_pre[
+            object_attribute_labels_pre[:, 0] != self.none_object_index
+        ]
+        object_attribute_labels_post = object_attribute_labels_post[
+            object_attribute_labels_post[:, 0] != self.none_object_index
+        ]
+
+        # calculate accuracy per attribute
+        average_accuracy = (
+            object_attribute_predictions_post == object_attribute_labels_post
+        ).sum(0) / object_attribute_predictions_post.shape[0]
+        # log accuracy per attribute
+        for acc, object_attribute_name in zip(average_accuracy, OBJECT_ATTRIBUTES):
+            self.log(f"val/accuracy/attribute_level/{object_attribute_name}", acc)
+        # log average overall accuracy
+        self.log(f"val/accuracy/average_accuracy", average_accuracy.mean())
+        # log accuracy of attributes that have changed
+        diff = object_attribute_labels_post != object_attribute_labels_pre
+        diff_accuracy = 0
+        if len(diff) > 0:
+            diff_accuracy = (
+                object_attribute_predictions_post[diff]
+                == object_attribute_labels_post[diff]
+            ).sum() / len(diff)
+        self.log(f"val/accuracy/diff_accuracy", diff_accuracy)
+
+    def configure_optimizers(self) -> torch.optim.Optimizer:
         return torch.optim.Adam(self.parameters(), lr=0.001)
